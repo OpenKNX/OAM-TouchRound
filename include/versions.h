8f763cad3c422da1b43dfc93424fe4ba4d959816
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2024-02-02 11:16:38 +0100

    Updated dependencies, especially switched LVGL to branch "release/v8.3".

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "8149e41"
-#define KNX_Version "52b5780"
+#define MAIN_Version "5aa9185"
+#define KNX_Version "9ab6b6c"
 #define MODULE_Common_Version "1.1.0+8a15569"
 #define MODULE_Common_ETS 17
