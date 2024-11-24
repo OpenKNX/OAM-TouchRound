d1271db7f45289a0457d6f2d70c477b196cbff07
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2024-11-24 18:30:05 +0100

    Color wheel no longer support in LVGL v9.

--- lib_dir/screen/src/screens/ui_Color.c
+++ lib_dir/screen/src/screens/ui_Color.c
@@ -10,11 +10,12 @@ void ui_Color_screen_init(void)
 ui_Color = lv_obj_create(NULL);
 lv_obj_clear_flag( ui_Color, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
 
-ui_ColorValue = lv_colorwheel_create(ui_Color,true);
-lv_obj_set_width( ui_ColorValue, 200);
-lv_obj_set_height( ui_ColorValue, 200);
-lv_obj_set_align( ui_ColorValue, LV_ALIGN_CENTER );
-lv_obj_set_style_arc_width(ui_ColorValue, 20, LV_PART_MAIN| LV_STATE_DEFAULT);
+// color wheel no longer supported
+// ui_ColorValue = lv_colorwheel_create(ui_Color,true);
+// lv_obj_set_width( ui_ColorValue, 200);
+// lv_obj_set_height( ui_ColorValue, 200);
+// lv_obj_set_align( ui_ColorValue, LV_ALIGN_CENTER );
+// lv_obj_set_style_arc_width(ui_ColorValue, 20, LV_PART_MAIN| LV_STATE_DEFAULT);
 
 ui_Label2 = lv_label_create(ui_Color);
 lv_obj_set_width( ui_Label2, LV_SIZE_CONTENT);  /// 1
