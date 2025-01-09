59218956d4eecd3ebdccb18a66eadb24deb322a8
Michael Geramb <lasso_riefen0m@icloud.com>
2025-01-09 22:22:40 +0100

    Remove temp files from git

--- include/versions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define MAIN_Version "06ba4ae"
-#define KNX_Version "2.1.2+764900e"
-#define MODULE_Common_Version "1.2.0+9f24e6e"
-#define MODULE_Common_Version_Major 1
-#define MODULE_Common_Version_Minor 2
-#define MODULE_Common_Version_Revision 0
-#define MODULE_Common_ETS 18
