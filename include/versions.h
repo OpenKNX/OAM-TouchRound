e84d35d5283aae64c80141d119a0ac040aa0761f
Michael Geramb <lasso_riefen0m@icloud.com>
2025-01-09 21:54:09 +0100

    Add restore scripts

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#define MAIN_Version "6c9426e"
-#define KNX_Version "2.1.0+84a0ce3"
-#define MODULE_Common_Version "1.2.0+8410b2a"
+#define MAIN_Version "06ba4ae"
+#define KNX_Version "2.1.2+764900e"
+#define MODULE_Common_Version "1.2.0+9f24e6e"
+#define MODULE_Common_Version_Major 1
+#define MODULE_Common_Version_Minor 2
+#define MODULE_Common_Version_Revision 0
 #define MODULE_Common_ETS 18
