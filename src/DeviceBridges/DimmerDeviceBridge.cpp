e7fa0b9f3d23556090f04b13e7d3d94e62af18d9
Michael Geramb <lasso_riefen0m@icloud.com>
2025-04-21 15:17:02 +0200

    Dimmer tested with live updates

--- src/DeviceBridges/DimmerDeviceBridge.cpp
+++ src/DeviceBridges/DimmerDeviceBridge.cpp
@@ -9,8 +9,10 @@ DimmerDeviceBridge::DimmerDeviceBridge(DetailDevicePage& detailDevicePage)
 void DimmerDeviceBridge::setup(uint8_t _channelIndex)
 {
     lv_label_set_text(_screen.label, _channel->getNameInUTF8());
-    _eventReleased =[](lv_event_t *e) { ((DimmerDeviceBridge*) lv_event_get_user_data(e))->released(); };
-    lv_obj_add_event_cb(_screen.slider, _eventReleased, LV_EVENT_RELEASED, this);
+    _eventSliderReleased =[](lv_event_t *e) { ((DimmerDeviceBridge*) lv_event_get_user_data(e))->sliderReleased(); };
+    lv_obj_add_event_cb(_screen.slider, _eventSliderReleased, LV_EVENT_RELEASED, this);
+    _eventSliderPressing = [](lv_event_t *e) { ((DimmerDeviceBridge*) lv_event_get_user_data(e))->sliderPressing(); };
+    lv_obj_add_event_cb(_screen.slider, _eventSliderPressing, LV_EVENT_PRESSING, this);
     _eventButtonPressed = [](lv_event_t *e) { ((DimmerDeviceBridge*) lv_event_get_user_data(e))->buttonClicked(); };
     lv_obj_add_event_cb(_screen.image, _eventButtonPressed , LV_EVENT_CLICKED, this);
 
@@ -20,14 +22,18 @@ void DimmerDeviceBridge::setup(uint8_t _channelIndex)
 
 DimmerDeviceBridge::~DimmerDeviceBridge()
 {
-    if (_eventReleased != nullptr)
-        lv_obj_remove_event_cb_with_user_data(_screen.slider, _eventReleased, this);
+    if (_eventSliderReleased != nullptr)
+        lv_obj_remove_event_cb_with_user_data(_screen.slider, _eventSliderReleased, this);
+    if (_eventSliderPressing != nullptr)
+        lv_obj_remove_event_cb_with_user_data(_screen.slider, _eventSliderPressing, this);
     if (_eventButtonPressed != nullptr)
         lv_obj_remove_event_cb_with_user_data(_screen.image, _eventButtonPressed, this);
 }
 
 void DimmerDeviceBridge::setBrightness(uint8_t brightness)
 {
+    if (_isSliderPressed)
+        return;
     lv_arc_set_value(_screen.slider, brightness);  
     ImageLoader::loadImage(_screen.image, _channel->mainFunctionImage().imageFile, _channel->mainFunctionImage().allowRecolor, brightness != 0); 
     updateText(); 
@@ -38,10 +44,19 @@ void DimmerDeviceBridge::updateText()
     lv_label_set_text_fmt(_screen.value, "%" LV_PRId32 "%%", lv_arc_get_value(_screen.slider));
 }
 
-void DimmerDeviceBridge::released()
+void DimmerDeviceBridge::sliderReleased()
 {    
+    _isSliderPressed = false;
     auto value = lv_arc_get_value(_screen.slider);
-    _channel->commandBrightness(this,  value);
+    _channel->commandBrightness(nullptr,  value);
+    updateText();
+}
+
+void DimmerDeviceBridge::sliderPressing()
+{    
+    _isSliderPressed = true;
+    auto value = lv_arc_get_value(_screen.slider);
+    _channel->commandBrightness(nullptr,  value);
     updateText();
 }
 
--- src/DeviceBridges/DimmerDeviceBridge.h
+++ src/DeviceBridges/DimmerDeviceBridge.h
@@ -7,16 +7,19 @@
 
 class DimmerDeviceBridge : public DimmerBridge
 {
-    lv_event_cb_t _eventReleased = nullptr;
+    lv_event_cb_t _eventSliderReleased = nullptr;
+    lv_event_cb_t _eventSliderPressing = nullptr;
     lv_event_cb_t _eventButtonPressed = nullptr;
     DetailDevicePage& _detailDevicePage;
     DimmerScreen& _screen = *DimmerScreen::instance;
+    bool _isSliderPressed = false;
 public:
     DimmerDeviceBridge(DetailDevicePage& detailDevicePage);
     virtual ~DimmerDeviceBridge() override;
     virtual void setup(uint8_t _channelIndex) override;
     virtual void setBrightness(uint8_t brightness) override;
-    void released();
+    void sliderReleased();
+    void sliderPressing();
     void buttonClicked();
     void updateText();
     virtual void mainFunctionValueChanged() override;
