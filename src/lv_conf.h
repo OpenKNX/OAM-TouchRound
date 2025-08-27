3ea6cb3ae25ce9aa8787ba59659b66344ecbfd8c
Michael Geramb <lasso_riefen0m@icloud.com>
2025-08-27 16:36:09 +0200

    Add font definition

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -1,6 +1,18 @@
 #pragma once
 #define LV_CONF_H
 
+#ifndef FONT_SMALL
+#define FONT_SMALL "NotoSans_Regular_16_CP_1252_ISO_8859_1_15"
+#endif
+
+#ifndef FONT_MEDIUM
+#define FONT_MEDIUM "NotoSans_Regular_24_CP_1252_ISO_8859_1_15"
+#endif
+
+#ifndef FONT_LARGE
+#define FONT_LARGE "NotoSans_Regular_40_CP_1252_ISO_8859_1_15"
+#endif
+
 #if LVGL_VERSION_MAJOR >= 9
 #include "../lib/OFM-TouchDisplay/src/lv_conf_v9.h"
 #else
